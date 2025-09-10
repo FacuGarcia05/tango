e8767375872e74ccb9404bd8435af396afea8679
FacuGarcia05 <garciafacundoezequiel05@gmail.com>
2025-09-10 15:46:09 -0300

    First commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="app-container">
+    <Header />
+    <main>
+      
+    </main>
+    <Footer />
+    </div>
+  );
+}
+
